0f61d6366133cf4eede1780760f57562f837a675
gileno <gileno.cr.duarte@gmail.com>
2022-10-11 15:40:00 -0300

    index view test and test for validate an element in an view, working

--- animais/testes/test_urls.py
+++ animais/testes/test_urls.py
@@ -1,9 +1,19 @@
-from django.test import TestCase
+from urllib import request, response
+from django.test import RequestFactory, TestCase
 from django.urls import reverse
 from animais.views import index
 
+
 class AnimaisURLSTestCase(TestCase):
-    """Testa se a home da aplicação utiliza a função index da view"""
-    def test_rota_url_utiliza_view_idex(self):
-        root = reverse('/')
-        self.assertEqual(root.func, index)
+
+    def setUp(self):
+        self.factory= RequestFactory()
+   
+    def test_validacacao_usabilidade_index(self):
+        """Testa se a home da aplicação utiliza a função index da view"""
+        request=self.factory.get('/')
+        with self.assertTemplateUsed('index.html'):
+
+            response=index(request)
+        
+            self.assertEqual(response.status_code, 200)
