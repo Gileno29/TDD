a795bc0dfae0658dd4186a44a3476dfd24b76aed
gileno <gileno.cr.duarte@gmail.com>
2022-10-11 14:14:11 -0300

    adicionando novo APP para testes

---
+++ idade.py
@@ -0,0 +1,6 @@
+def descobrir(ano_nascimento,ano):
+    return ano-ano_nascimento
+
+assert descobrir(1991,2050)==59
+
+print(descobrir(1996, 2060))
--- setup/teste.py
+++ setup/teste.py
@@ -2,16 +2,23 @@ from cgi import test
 from select import select
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 class AnimaisTestCase(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome("/home/gileno/Documents/estudando/TDD/chromedriver")
     def tearDown(self):
         self.browser.quit()
+  
 
-    def test_para_verificar_se_a_janela_do_browser_esta_abrindo(self):
-        self.browser.get(self.live_server_url)
+    def test_buscando_um_novo_animal(self):
+        """
+            Teste se um usuário encontra um animal na pesquisa
+        """
+        self.browser.get(self.live_server_url + '/')
+        brand_element  = self.browser.find_element_by_css_selector('.navbar')
+        self.assertEqual('Busca Animal', brand_element.text)
+        pass
 
-    def test_deu_ruim(self):
-        """Teste de exemplo de erro"""
-        self.fail('teste falhou  deu ruim mesmo')
+    
+    #find_element(By.CSS_SELECTOR, 'exemplo')
